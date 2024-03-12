e61ba916610b67a38c2460fdb14e42639ddf3223
Radvan Alexandru <rip777ton@gmail.com>
2024-03-12 18:19:06 +0200

    Add connection to db.

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("library start");
+const db = require("./config/dbConfig");
+const q = "SELECT * FROM books";
+db.query(q, (err, data) => {
+  if (err) console.log(err);
+  console.log(data);
+});
