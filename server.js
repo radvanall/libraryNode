94da2e501a1c09f19919afeacf916cfe8a9fca88
Radvan Alexandru <rip777ton@gmail.com>
2024-03-14 19:08:42 +0200

    Add delete and update paths

--- server.js
+++ server.js
@@ -23,6 +23,29 @@ app.post("/books", (req, res) => {
     return res.json("The book has been created");
   });
 });
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q =
+    "update books set `title`=?, `description`=?, `cover`=?, `author`=? where id=?";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.cover,
+    req.body.author,
+  ];
+  db.query(q, [...values, bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been updated successfully");
+  });
+});
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "delete from books  where id=?";
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been deleted successfully");
+  });
+});
 app.listen(3500, () => {
   console.log("Connected to backend");
 });
