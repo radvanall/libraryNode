22c6603077758526e5e2aec0d505f9f2944aba9d
Radvan Alexandru <rip777ton@gmail.com>
2024-04-22 19:10:42 +0300

    Instal multer package for working with multipart/form-data. Create to functions that work with multer.

---
+++ src/middleware/multerErrorHandler.js
@@ -0,0 +1,16 @@
+const multer = require("multer");
+const multerErrorHandler = function (err, req, res, next) {
+  if (err instanceof multer.MulterError) {
+    // handle file size error
+    if (err.code === "LIMIT_FILE_SIZE")
+      return res.status(400).send({ error: err.message });
+    // handle unexpected file error
+    if (err.code === "LIMIT_UNEXPECTED_FILE")
+      return res.status(400).send({ error: err.message });
+    // handle unexpected field key error
+    if (err.code === "LIMIT_FIELD_KEY")
+      return res.status(400).send({ error: err.message });
+  }
+  res.status(400).end();
+};
+module.exports = multerErrorHandler;
---
+++ src/middleware/multipartMulter.js
@@ -0,0 +1,66 @@
+const multer = require("multer");
+
+const multipartMulter = (folder) => {
+  const storage = multer.diskStorage({
+    destination(req, res, cb) {
+      cb(null, folder);
+    },
+    filename(req, file, cb) {
+      cb(
+        null,
+        new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+      );
+    },
+  });
+
+  const types = ["image/png", "image/jpeg", "image/jpg"];
+  const maxSize = 10 * 1024 * 1024;
+
+  const fileFilter = (req, file, cb) => {
+    if (types.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+
+  return multer({
+    storage,
+    fileFilter,
+    limits: {
+      fileSize: maxSize,
+    },
+  });
+};
+module.exports = multipartMulter;
+
+// const storage = multer.diskStorage({
+//   destination(req, res, cb) {
+//     cb(null, "images/");
+//   },
+//   filename(req, file, cb) {
+//     cb(
+//       null,
+//       new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+//     );
+//   },
+// });
+
+// const types = ["image/png", "image/jpeg", "image/jpg"];
+// const maxSize = 10 * 1024 * 1024;
+
+// const fileFilter = (req, file, cb) => {
+//   if (types.includes(file.mimetype)) {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
+
+// module.exports = multer({
+//   storage,
+//   fileFilter,
+//   limits: {
+//     fileSize: maxSize,
+//   },
+// });
