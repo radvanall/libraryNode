b65930aafd4632ccfac19cae65a0195ab8aa663f
Radvan Alexandru <rip777ton@gmail.com>
2024-05-20 21:25:38 +0300

    Extract the setGenre function into a distinct function

--- src/utils/setGenres.js
+++ src/utils/setGenres.js
@@ -1,17 +1,31 @@
-const setGeneres = (data) => {
+const setGenre = (book) => {
+  if (book.genres) {
+    const gens = book.genres.split(",");
+    const gen = gens.map((g) => {
+      const newG = g.split("|");
+      return {
+        genre: newG[0],
+        genreId: parseInt(newG[1]),
+      };
+    });
+    book.genres = gen;
+  }
+};
+const setGenres = (data) => {
   data.forEach((book) => {
-    if (book.genres) {
-      const gens = book.genres.split(",");
-      const gen = gens.map((g) => {
-        const newG = g.split("|");
-        return {
-          genre: newG[0],
-          genreId: parseInt(newG[1]),
-        };
-      });
-      book.genres = gen;
-    }
+    setGenre(book);
+    // if (book.genres) {
+    //   const gens = book.genres.split(",");
+    //   const gen = gens.map((g) => {
+    //     const newG = g.split("|");
+    //     return {
+    //       genre: newG[0],
+    //       genreId: parseInt(newG[1]),
+    //     };
+    //   });
+    //   book.genres = gen;
+    // }
   });
   return data;
 };
-module.exports = setGeneres;
+module.exports = { setGenres, setGenre };
