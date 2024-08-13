d0b4bbae5edfbf55cd24c0b39decef56dd5ab7bb
Radvan Alexandru <rip777ton@gmail.com>
2024-08-13 22:48:13 +0300

    Update the sql query.

--- src/utils/getBooksRowCount.js
+++ src/utils/getBooksRowCount.js
@@ -1,7 +1,7 @@
 const db = require("../../config/dbConfig");
-const getBooksRowCount = async (ignoredGenres) => {
+const getBooksRowCount = async (ignoredGenres, searchPattern) => {
   let q = `SELECT COUNT(*) as totalCount FROM(
-    SELECT DISTINCT b.id from  books b  left join books_genres bg on b.id=bg.book_id 
+    SELECT DISTINCT b.id from  books b  left join books_genres bg on b.id=bg.book_id   WHERE b.title like ? GROUP BY b.id
          HAVING b.id in (SELECT b.id FROM books b  left join books_genres bg on b.id=bg.book_id  `;
   if (Array.isArray(ignoredGenres) && ignoredGenres.length > 0) {
     console.log("a");
@@ -10,7 +10,7 @@ const getBooksRowCount = async (ignoredGenres) => {
       " where bg.genre_id not in (" + placeholders + ") OR bg.genre_id IS NULL";
   }
   q += ")) as t";
-  const [res] = await db.query(q, ignoredGenres);
+  const [res] = await db.query(q, [searchPattern, ...ignoredGenres]);
   console.log(res);
   return res[0].totalCount;
 };
