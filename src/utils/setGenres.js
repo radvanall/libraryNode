07259b934c92844394d873466f1e7bb9461a284b
Rădvan Alexandru <rip777ton@gmail.com>
2024-11-14 23:24:24 +0200

    Remove comments

--- src/utils/setGenres.js
+++ src/utils/setGenres.js
@@ -14,17 +14,6 @@ const setGenre = (book) => {
 const setGenres = (data) => {
   data.forEach((book) => {
     setGenre(book);
-    // if (book.genres) {
-    //   const gens = book.genres.split(",");
-    //   const gen = gens.map((g) => {
-    //     const newG = g.split("|");
-    //     return {
-    //       genre: newG[0],
-    //       genreId: parseInt(newG[1]),
-    //     };
-    //   });
-    //   book.genres = gen;
-    // }
   });
   return data;
 };
