1ac59ba5e775f3881a8f79b0a0f522a681bc1510
Rădvan Alexandru <rip777ton@gmail.com>
2024-12-05 23:21:16 +0200

    Remove comments

--- server.js
+++ server.js
@@ -1,14 +1,3 @@
-// const express = require("express");
-// const app = express();
-
-// app.use(express.json());
-
-// app.use("/books", require("./routes/bookRoutes"));
-// app.use("/users", require("./routes/userRoutes"));
-
-// app.listen(3500, () => {
-//   console.log("Connected to backend");
-// });
 const app = require("./src/app");
 
 app.listen(3500, () => {
