307c98876690f5be8afb78b39e86b537134651f6
Rădvan Alexandru <rip777ton@gmail.com>
2025-01-09 23:08:24 +0200

    Remove unused code

--- src/model/comment.js
+++ src/model/comment.js
@@ -10,7 +10,6 @@ const getCommentByBookId = async (bookId, totalComments, limit, page) => {
   const q = `select c.id,c.comment,c.user_id, u.login, u.avatar from comments c left join users u on c.user_id=u.id where book_id=? order by c.id DESC LIMIT ? OFFSET ?`;
   const [data] = await db.query(q, [bookId, limit, offset]);
   return createPaginatedResult(page, limit, offset, totalComments, data);
-  // return data;
 };
 const getTotalNrOfCommentsByUserId = async (userId) => {
   const q =
@@ -30,7 +29,6 @@ const getCommentByUserId = async (userId, totalComments, limit, page) => {
     "select u.login,b.id as bookId,b.title,b.cover,c.comment, c.id as commentId from users u left join comments c on c.user_id=u.id left join books b on b.id=c.book_id where u.id=? LIMIT ? OFFSET ?";
   const [data] = await db.query(q, [userId, limit, offset]);
   return createPaginatedResult(page, limit, offset, totalComments, data);
-  // return data;
 };
 
 const editComment = async (values) => {
